a8cbc5bf6379aa7a90057146eb3afba97641a8e3
carlosmc23 <kenshinmc23@gmail.com>
2020-05-11 10:09:46 -0400

    implement basic mapper

--- src/main/java/org/fundacionjala/trello/utils/Mapper.java
+++ src/main/java/org/fundacionjala/trello/utils/Mapper.java
@@ -4,11 +4,21 @@ package org.fundacionjala.trello.utils;
 import io.restassured.response.Response;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Gropus utility methods to map data.
  */
 public final class Mapper {
+
+    private static final String STORED_VALUE_PATTERN = "\\{[0-9a-zA-Z._\\-\\[\\]]+}";
+    private static final String EMPTY_KEY = "empty";
+    private static final String EMPTY_STRING = "";
+    private static final String CAP_REGEX = "[{}]";
+    private static final String DOT_REGEX = "\\.";
+    private static final int SPLIT_LIMIT = 2;
 
     /**
      * Private constructor for Mapper utility class.
@@ -25,8 +35,18 @@ public final class Mapper {
      * @return string mapped.
      */
     public static String replaceData(final String content, final Map<String, Response> responses) {
-        // To do.
-        return content;
+        Pattern pattern = Pattern.compile(STORED_VALUE_PATTERN);
+        Matcher matcher = pattern.matcher(content);
+        StringBuffer result = new StringBuffer();
+        while (matcher.find()) {
+            String value = replaceValue(matcher.group().replaceAll(CAP_REGEX, EMPTY_STRING), responses);
+            if (value == null) {
+                continue;
+            }
+            matcher.appendReplacement(result, value);
+        }
+        matcher.appendTail(result);
+        return result.toString();
     }
 
     /**
@@ -38,7 +58,25 @@ public final class Mapper {
      */
     public static Map<String, String> replaceData(final Map<String, String> content,
                                                   final Map<String, Response> responses) {
-        // To do.
-        return content;
+        return content.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> replaceData(e.getValue(), responses)));
+    }
+
+    /**
+     * Replaces values from response or account information.
+     *
+     * @param value     original value.
+     * @param responses response objects stored.
+     * @return value replaced.
+     */
+    private static String replaceValue(final String value, final Map<String, Response> responses) {
+        if (value.equals(EMPTY_KEY)) {
+            return EMPTY_STRING;
+        }
+        String[] values = value.split(DOT_REGEX, SPLIT_LIMIT);
+        Response response = responses.get(values[0]);
+        String jsonPath = values[1];
+        return response.jsonPath().getString(jsonPath);
+
     }
 }
