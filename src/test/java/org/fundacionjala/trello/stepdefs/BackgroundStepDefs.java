4d5553b0238fb7ecd99710db33d642bf6c65b562
nanrehd <jdhpp_perezotmail.com>
2020-05-13 16:11:45 -0400

    adding base for testing list endpoint

--- src/test/java/org/fundacionjala/trello/stepdefs/BackgroundStepDefs.java
+++ src/test/java/org/fundacionjala/trello/stepdefs/BackgroundStepDefs.java
@@ -1,9 +1,11 @@
 package org.fundacionjala.trello.stepdefs;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.fundacionjala.trello.context.Context;
 import org.fundacionjala.trello.entities.Board;
 import org.fundacionjala.trello.entities.Label;
+import org.fundacionjala.trello.entities.List;
 import org.fundacionjala.trello.entities.Organization;
 
 public class BackgroundStepDefs {
@@ -12,17 +14,19 @@ public class BackgroundStepDefs {
     private Organization org;
     private Board board;
     private Label label;
+    private List list;
 
-    public BackgroundStepDefs(final Context context, final Organization org, final Board board, final Label label) {
+    public BackgroundStepDefs(final Context context, final Organization org, final Board board, final Label label,
+                              final List list) {
         this.context = context;
         this.org = org;
         this.board = board;
         this.label = label;
+        this.list = list;
     }
 
     /**
      * Creates an organization by default.
-     *
      */
     @When("I have an organization created")
     public void haveAnOrgCreated() {
@@ -43,5 +47,13 @@ public class BackgroundStepDefs {
     @When("I have a label created")
     public void iHaveALabelCreated() {
         label.createNew();
+    }
+
+    /**
+     * Creates a new list by default.
+     */
+    @And("I have a list created")
+    public void iHaveAListCreated() {
+        list.createNew();
     }
 }
