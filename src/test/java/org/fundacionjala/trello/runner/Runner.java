13e0e6c4d3306a45c99651b3d5869202b7ebef80
nanrehd <jdhpp_perezotmail.com>
2020-05-13 10:10:15 -0400

    first configurations for parallel exec

--- src/test/java/org/fundacionjala/trello/runner/Runner.java
+++ src/test/java/org/fundacionjala/trello/runner/Runner.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 
 /**
  * Cucumber TestNG runner class.
@@ -30,5 +31,11 @@ public class Runner extends AbstractTestNGCucumberTests {
     @AfterTest
     public void afterAllScenarios() {
         // Code executed after features execution.
+    }
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
     }
 }
