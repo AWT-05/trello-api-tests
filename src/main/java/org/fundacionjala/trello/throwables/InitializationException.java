03a5bd003ac91e6f270e2558a52e8400bcc94baf
Snip-77 <mau.oroza1@gmail.com>
2020-05-12 21:12:30 -0400

    Clean check

--- src/main/java/org/fundacionjala/trello/throwables/InitializationException.java
+++ src/main/java/org/fundacionjala/trello/throwables/InitializationException.java
@@ -2,7 +2,7 @@ package org.fundacionjala.trello.throwables;
 
 public class InitializationException extends ExceptionInInitializerError {
 
-    public InitializationException(String message) {
+    public InitializationException(final String message) {
         super(message);
         System.exit(1);
     }
--- src/main/java/org/fundacionjala/trello/throwables/NotDefinedValueException.java
+++ src/main/java/org/fundacionjala/trello/throwables/NotDefinedValueException.java
@@ -2,7 +2,7 @@ package org.fundacionjala.trello.throwables;
 
 public class NotDefinedValueException extends ExceptionInInitializerError {
 
-        public NotDefinedValueException(String message) {
+        public NotDefinedValueException(final String message) {
             super(message);
         }
 }
