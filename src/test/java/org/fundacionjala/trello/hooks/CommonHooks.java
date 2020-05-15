2d92d22004c204a2729a300435053c6a2cb6c8b5
Snip-77 <mau.oroza1@gmail.com>
2020-05-15 09:03:55 -0400

    Cleaning code

--- src/test/java/org/fundacionjala/trello/hooks/CommonHooks.java
+++ src/test/java/org/fundacionjala/trello/hooks/CommonHooks.java
@@ -2,7 +2,6 @@ package org.fundacionjala.trello.hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.fundacionjala.trello.client.RequestManagerRestAssured;
 import org.fundacionjala.trello.context.Context;
 import org.fundacionjala.trello.utils.RequestSpecUtils;
 import org.testng.SkipException;
@@ -33,7 +32,6 @@ public class CommonHooks {
      */
     @Before(order = BUILD_REQ_SEC_ORDER_VALUE)
     public void buildDefaultReqSpec() {
-        RequestManagerRestAssured.displayFiltersData(); // Only for help
         context.setReqSpec(RequestSpecUtils.build());
     }
 
